71548617319821846b8c14a4c74f4d16a90e1c70
Cameron Smith <cameron.ray.smith@gmail.com>
2025-02-20 22:40:20 -0500

    fix(lazyvim): import `lang/rust.nix` extra
    Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- lazyvim/default.nix
+++ lazyvim/default.nix
@@ -33,6 +33,7 @@ in
     ./extras/lang/nix.nix
     ./extras/lang/prisma.nix
     ./extras/lang/python.nix
+    ./extras/lang/rust.nix
     ./extras/lang/svelte.nix
     ./extras/lang/tailwind.nix
     ./extras/lang/typescript.nix
