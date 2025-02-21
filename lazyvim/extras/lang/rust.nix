1359caec8e069e2b9b4b9e0de4d577a42799406c
Cameron Smith <cameron.ray.smith@gmail.com>
2025-02-21 08:50:33 -0500

    fix(lang): add nixpkgs overrides to crates.nvim
    Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- lazyvim/extras/lang/rust.nix
+++ lazyvim/extras/lang/rust.nix
@@ -35,19 +35,28 @@ in
         # )
         #
         # NOTE: crates-nvim recent update required
-        crates-nvim
-        # but fails to load crates.null-ls related to null-ls-nvim
-        # (pkgs.vimUtils.buildVimPlugin {
-        #   pname = "crates.nvim";
-        #   version = "2025-02-20";
-        #   src = pkgs.fetchFromGitHub {
-        #     owner = "saecki";
-        #     repo = "crates.nvim";
-        #     rev = "1803c8b5516610ba7cdb759a4472a78414ee6cd4";
-        #     sha256 = "0bqcdsbhs1ab51nmqd3cx7p6nlpmrjj0a53hax9scpqzr23nvr66";
-        #   };
-        #   meta.homepage = "https://github.com/saecki/crates.nvim/";
-        # })
+        # crates-nvim
+        # but fails to load crates.null-ls related to null-ls-nvim or none-ls-nvim
+        # without overrides from 
+        # https://github.com/NixOS/nixpkgs/blob/5092f59914b45d6e80e1650324eac988e7520907/pkgs/applications/editors/vim/plugins/overrides.nix#L891-L897
+        (pkgs.vimUtils.buildVimPlugin {
+          pname = "crates.nvim";
+          version = "2025-02-20";
+          src = pkgs.fetchFromGitHub {
+            owner = "saecki";
+            repo = "crates.nvim";
+            rev = "1803c8b5516610ba7cdb759a4472a78414ee6cd4";
+            sha256 = "0bqcdsbhs1ab51nmqd3cx7p6nlpmrjj0a53hax9scpqzr23nvr66";
+          };
+          meta.homepage = "https://github.com/saecki/crates.nvim/";
+          # see overrides in nixpkgs/pkgs/applications/editors/vim/plugins/overrides.nix
+          checkInputs = [
+            none-ls-nvim
+          ];
+          dependencies = [
+            plenary-nvim
+          ];
+        })
         rustaceanvim
         clangd_extensions-nvim
       ];
